e489c86ae6c9ddbd3135b784fea515edd46df510
Lucas Gruner <lucas.gruner@gmail.com>
2023-02-22 16:39:19 +0100

    refactoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,5 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
@@ -13,7 +8,7 @@ class ReviewsController < ApplicationController
       @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
